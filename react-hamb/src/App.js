b47f7bbd3292f54277baa681d223af23d1e7ce88
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-12-01 10:46:30 +0100

    Update App.js

--- react-hamb/src/App.js
+++ react-hamb/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import PersonSetState from "./stuff/PersonSetState"
+import PersonUseStateHook from "./stuff/PersonUseStateHook"
+import {Button, Container, Row} from "react-bootstrap"
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <Row className="">
+           <PersonSetState />
+           <hr /> 
+           <PersonUseStateHook />
+       
+      </Row>
+    )
+  }
 }
-
-export default App;
